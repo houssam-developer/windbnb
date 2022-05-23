859412ae0df33c77eea747347a9c637340dcb3cd
houssam-dev <developpeur.pro.ma@gmail.com>
2022-05-23 08:18:40 +0100

    feat: add toggle between results of location and guests

--- src/components/SearchMenu.jsx
+++ src/components/SearchMenu.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import staysData from '../data/stays.json';
 
-const NEGATIF_FULL = '-100%'
-const POSITIF_FULL = '100%'
+const NEGATIF_FULL = '-100%';
+const POSITIF_FULL = '100%';
+const TARGET_INPUT_LOCATION = 'location';
+const TARGET_INPUT_GUESTS = 'guests';
 
 function SearchMenu() {
 	const [locationValue, setLocationValue] = useState('Helsinki, Finland');
@@ -41,6 +43,23 @@ function SearchMenu() {
 	)
 
 	function SearchModal({ fnOnClose }) {
+		const [displayLocationResults, setDisplayLocationResults] = useState('hidden');
+		const [displayGuestsResults, setDisplayGuestsResults] = useState('hidden');
+
+		function updateDisplayResult(targetInput) {
+			if (targetInput === TARGET_INPUT_LOCATION) {
+				setDisplayGuestsResults('hidden');
+				setDisplayLocationResults('');
+				return;
+			}
+
+			if (targetInput === TARGET_INPUT_GUESTS) {
+				setDisplayLocationResults('hidden');
+				setDisplayGuestsResults('');
+				return;
+			}
+		}
+
 		return (
 			<div className={`absolute top-[${topVal}] right-0 left-0 p-4 bg-white flex flex-col min-h-[90vh] gap-6`}>
 				{/* Edit actions */}
@@ -55,20 +74,22 @@ function SearchMenu() {
 
 				{/* Locations - Guests */}
 
-
 				<div className='shadow rounded-xl border-[1px] focus-within:border-orange-300 w-full md:w-[768px] mx-auto'>
 					<div className='flex flex-col border-b-[1px] py-2 px-4 gap-2 '>
 						<label className='text-[9px] uppercase font-bold'>location</label>
-						<input className='text-sm focus:outline-none border-none' type="text" placeholder='Helsinki, Finland' />
+						<input className='text-sm focus:outline-none border-none' type="text"
+							placeholder='Helsinki, Finland' onClick={() => updateDisplayResult(TARGET_INPUT_LOCATION)} />
 					</div>
 					<div className='flex flex-col border-b-[1px] py-2 px-4 gap-2'>
 						<label className='text-[9px] uppercase font-bold'>guests</label>
-						<input className='text-sm focus:outline-none border-none' type="tel" placeholder='Add guests' />
+						<input className='text-sm focus:outline-none border-none' type="tel"
+							placeholder='Add guests' onClick={() => updateDisplayResult(TARGET_INPUT_GUESTS)} />
 					</div>
 				</div>
 
 				{/* Results */}
-				<div className=' p-4 flex flex-col gap-6 max-h-[45vh] overflow-auto w-full md:md:w-[768px] mx-auto'>
+
+				<div className={`${displayLocationResults} p-4 flex flex-col gap-6 max-h-[45vh] overflow-auto w-full md:md:w-[768px] mx-auto`}>
 					{
 						staysData.map(it =>
 							<button key={it.title} className='flex gap-2 text-[#4f4f4f]'>
@@ -80,7 +101,7 @@ function SearchMenu() {
 						)
 					}
 				</div>
-				<div className='hidden bg-[#f9f9f9] shadow border-2 border-slate-200 rounded-xl p-4 grid grid-cols-3 place-items-center gap-6 overflow-auto w-full md:md:w-[768px] mx-auto'>
+				<div className={`${displayGuestsResults} bg-[#f9f9f9] shadow border-2 border-slate-200 rounded-xl p-4 grid grid-cols-3 place-items-center gap-6 overflow-auto w-full md:md:w-[768px] mx-auto`}>
 					<button className="border-b-2 border-[#EB5757E5] p-2 px-4 text-[#4f4f4f] hover:border-orange-300 hover:text-[#111]">1</button>
 					<button className="border-b-2 border-[#EB5757E5] p-2 px-4 text-[#4f4f4f] hover:border-orange-300 hover:text-[#111]">2</button>
 					<button className="border-b-2 border-[#EB5757E5] p-2 px-4 text-[#4f4f4f] hover:border-orange-300 hover:text-[#111]">3</button>
@@ -93,7 +114,7 @@ function SearchMenu() {
 					<button className="border-b-2 border-[#EB5757E5] p-2 px-4 text-[#4f4f4f] hover:border-orange-300 hover:text-[#111]">10</button>
 				</div>
 
-				<button className='flex gap-2 self-center bg-[#EB5757E5] rounded-2xl text-white px-6 py-3 mt-1' type='submit'>
+				<button className='mt-auto flex gap-2 self-center bg-[#EB5757E5] rounded-2xl text-white px-6 py-3 mt-1' type='submit'>
 					<svg style={{ width: '24px', height: '24px', fill: '#F2F2F2' }} viewBox="0 0 24 24">
 						<path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" />
 					</svg>
